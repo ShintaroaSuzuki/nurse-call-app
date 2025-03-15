2b84ab8786e24f47c52c418ecbcb5966423201f4
shintaroasuzuki <shinbo.dorapion19971221@gmail.com>
2025-03-15 01:15:08 +0000

    feat: :sparkles: update .gitignore

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/ec2-user/projects/nurse-call-app/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/ec2-user/projects/nurse-call-app/src/app/layout":["static/media/a34f9d1faa5f3315-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DiHLq9P2M6mVg7G17dfvN/aoJzUx8tZXZk2f8tmqFrg=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"72orIc+ScorWvs06u6BX5pPNRdWJtE4WJDeWXdRLSTo=\"}"
